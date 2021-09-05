167cc91c78e1953ebb6d3e27552e7e3ae9a70842
wangfei_pro <51104860@163.com>
2021-09-05 14:29:19 +0800

    暂存部分更改

---
+++ 3_redux完整版/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Count from './components/Count'
+import 'antd/dist/antd.css'
+import './index.css'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Count />
+      </div>
+    )
+  }
+}
---
+++ 3_redux完整版/redux/count_reducer.js
@@ -0,0 +1,23 @@
+/* 
+  创建一个为Count组件服务的reducer,reducer的本质就是一个函数
+  reducer函数会接到两个参数，之前的状态与动作对象
+*/
+import { DECREMENT, INCREMENT } from './constant'
+const initState = 0
+// 采用形参默认值的方式进行状态的初始化
+export default function countReducer(preState = initState, action) {
+  // console.log('preState:', preState, 'action:', action)
+
+  // if (preState === undefined) preState = 0
+  const { type, data } = action
+  // 根据type决定如何加工数据
+  switch (type) {
+    case INCREMENT:
+      return preState + data
+    case DECREMENT:
+      return preState - data
+
+    default:
+      return preState
+  }
+}
